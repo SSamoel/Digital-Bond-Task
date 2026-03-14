cfda2163535f62a34c714c6da84b14f390e2c9dd
SSamoel <ssamoel@outlook.com>
2026-03-14 12:06:59 +0200

    General changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',
+    title: 'Digital Bond | Home',
+    loadComponent: () => import('./pages/homepage/homepage').then(m => m.Homepage)},
+  { path: 'done' ,
+    title: 'Digital Bond | Success',
+    loadComponent: () => import('./pages/success/success').then(m => m.Success)},
+  { path: '**' , redirectTo: '' , pathMatch: 'full'}
+];
