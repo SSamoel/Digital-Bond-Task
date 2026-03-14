10ecb91d817c76f12076891498a24a494fdb5c11
SSamoel <ssamoel@outlook.com>
2026-03-14 21:27:27 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '',
-    title: 'Digital Bond | Home',
+    title: 'Digital Bond | HomePage',
     loadComponent: () => import('./pages/homepage/homepage').then(m => m.Homepage)},
   { path: 'done' ,
     title: 'Digital Bond | Success',
