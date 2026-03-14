51de36d49e8c078191dcae84a811a858c836c121
SSamoel <ssamoel@outlook.com>
2026-03-14 12:02:09 +0200

    Create a navbar with links leading to the required section on the landing page.

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser, NgClass } from '@angular/common';
+import { Component, HostListener, inject, PLATFORM_ID, signal } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
 
+  // هنا بجيب الهوية هل انا عند المستخدم في المتصفح ولا علي السيرفر
+  private platformId = inject(PLATFORM_ID);
+
+  isScrolled = signal(false);
+  isMenuOpen = signal(false);
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // بسأل هل انا عند المستخدم في المتصفح
+    if (isPlatformBrowser(this.platformId)) {
+      // لو اه فانا كدة هقدر ا ستخدم window , بقوله هاتلي الرقم اللي هو ععدد البكسلات اللي اتعملها scroll
+      const scrollOffset = window.pageYOffset
+      // بسال ال document element نفسه أنت نازل تحت قد اية
+      || document.documentElement.scrollTop;
+      this.isScrolled.set(scrollOffset > 50);
+    }
+  }
+
+  toggleMenu(){
+    this.isMenuOpen.update(value => !value);
+  }
 }
